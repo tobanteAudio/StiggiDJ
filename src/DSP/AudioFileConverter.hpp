d2da606eadcc7c02b6b74e60aa12e885fa051bfb
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-06-13 20:14:02 +0200

    Convert to wav before bpm detection

---
+++ src/DSP/AudioFileConverter.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <juce_audio_formats/juce_audio_formats.h>
+#include <juce_core/juce_core.h>
+
+namespace ta
+{
+
+struct AudioFileConverterOptions
+{
+    juce::File source{};
+    juce::File destination{};
+};
+
+struct AudioFileConverterResult
+{
+    bool success{false};
+    juce::String errorMessage{};
+};
+
+struct AudioFileConverter
+{
+    using Options = AudioFileConverterOptions;
+    using Result  = AudioFileConverterResult;
+
+    AudioFileConverter();
+
+    template<typename DestinationFormat>
+    auto convert(Options const& options) -> Result;
+
+private:
+    juce::AudioFormatManager _formatManager;
+};
+
+inline AudioFileConverter::AudioFileConverter() { _formatManager.registerBasicFormats(); }
+
+template<typename DestinationFormat>
+inline auto AudioFileConverter::convert(Options const& options) -> Result
+{
+    auto reader = std::unique_ptr<juce::AudioFormatReader>(_formatManager.createReaderFor(options.source));
+    if (reader == nullptr) { return Result{false, "Invalid file"}; }
+
+    DestinationFormat format{};
+    auto const sr   = reader->sampleRate;
+    auto const ch   = reader->numChannels;
+    auto const bits = reader->bitsPerSample;
+
+    auto out    = std::make_unique<juce::FileOutputStream>(options.destination);
+    auto writer = std::unique_ptr<juce::AudioFormatWriter>(format.createWriterFor(out.get(), sr, ch, bits, {}, 0));
+
+    if (writer == nullptr) { return Result{false, "Failed to convert to wav"}; }
+    out.release();  // writer took ownership
+
+    writer->writeFromAudioReader(*reader, 0, reader->lengthInSamples);
+    writer->flush();
+
+    return Result{true, {}};
+}
+
+}  // namespace ta
--- src/DSP/BeatTrack.cpp
+++ src/DSP/BeatTrack.cpp
@@ -1,15 +1,23 @@
 #include "BeatTrack.hpp"
 
+#include "DSP/AudioFileConverter.hpp"
+
 namespace ta
 {
 auto beatTrack(juce::File const& audioFile) -> BeatTrackResult
 {
+    juce::TemporaryFile tmpFile{};
+    AudioFileConverter converter{};
+    auto const convertOptions = AudioFileConverterOptions{audioFile, tmpFile.getFile()};
+    auto const convertResult  = converter.convert<juce::WavAudioFormat>(convertOptions);
+    if (!convertResult.success) { return BeatTrackResult{convertResult.errorMessage}; }
+
     auto const* pythonPath = "/home/tobante/Developer/tobanteAudio/StiggiDJ/venv/bin/python3";
     auto const scriptDir   = juce::File{"/home/tobante/Developer/tobanteAudio/StiggiDJ/src/Scripts"};
     auto const scriptPath  = scriptDir.getChildFile("bpm_track.py");
 
     juce::ChildProcess process{};
-    auto processArgs = juce::StringArray{pythonPath, scriptPath.getFullPathName(), audioFile.getFullPathName()};
+    auto processArgs = juce::StringArray{pythonPath, scriptPath.getFullPathName(), tmpFile.getFile().getFullPathName()};
     if (!process.start(processArgs)) { return BeatTrackResult{"Failed to start"}; }
     if (!process.waitForProcessToFinish(10'000)) { return BeatTrackResult{"Timeout"}; }
 
